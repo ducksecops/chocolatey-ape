29c666e169dbc93a945fd59892c47909e3de24e5
ducksecops <daniel@ducksecops.uk>
2020-01-21 10:25:50 +0000

    Initial commit for Chocolatey Ape package 3.1.256

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://www.helicontech.com/download/ape/Helicon_Ape_0256.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+
+  softwareName  = 'Helicon Ape module for IIS*'
+
+  checksum      = 'BBFF1279189808FA075CB64B75312E14437A00640351171FAD8B96554959A58A'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
